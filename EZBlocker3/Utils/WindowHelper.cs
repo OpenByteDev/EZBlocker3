5f0816943cf96d4b32fb526726f974fd9f13f4dc
OpenByte <development.openbyte@gmail.com>
2020-09-30 16:51:01 +0200

    Autosize update windows

---
+++ EZBlocker3/Utils/WindowHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+
+namespace EZBlocker3.Utils {
+    internal static class WindowHelper {
+
+        public static void ApplySizeToContentFix(Window window) {
+            void Handler(object sender, EventArgs eventArgs) {
+                window.InvalidateMeasure();
+                window.SourceInitialized -= Handler;
+            }
+
+            window.SourceInitialized += Handler;
+        }
+
+        public static readonly DependencyProperty ApplySizeToContentFixProperty =
+            DependencyProperty.RegisterAttached(
+                "ApplySizeToContentFix",
+                typeof(bool),
+                typeof(WindowHelper),
+                new PropertyMetadata(OnApplySizeToContentFixChanged));
+
+        public static bool GetApplySizeToContentFix(Window window) {
+            return (bool)window.GetValue(ApplySizeToContentFixProperty);
+        }
+
+        public static void SetApplySizeToContentFix(Window window, bool value) {
+            window.SetValue(ApplySizeToContentFixProperty, value);
+        }
+
+        private static void OnApplySizeToContentFixChanged(DependencyObject obj, DependencyPropertyChangedEventArgs eventArgs) {
+            bool newValue = (bool)eventArgs.NewValue;
+
+            if (newValue && obj is Window window)
+                ApplySizeToContentFix(window);
+        }
+
+    }
+}
