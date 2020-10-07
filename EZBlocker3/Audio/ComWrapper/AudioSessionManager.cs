e76513a148f310584141cf404aee003e5635c144
OpenByte <development.openbyte@gmail.com>
2020-10-07 20:54:44 +0200

    Remove unused code

--- EZBlocker3/Audio/ComWrapper/AudioSessionManager.cs
+++ EZBlocker3/Audio/ComWrapper/AudioSessionManager.cs
@@ -8,10 +8,6 @@ namespace EZBlocker3.Audio.ComWrapper {
 
         public AudioSessionManager(IAudioSessionManager2 sessionManager) : base(sessionManager) { }
 
-        public IEnumerable<AudioSession> GetSessions() {
-            return new Enumerable(this);
-        }
-
         public AudioSessionCollection GetSessionCollection() {
             IAudioSessionEnumerator? sessionEnumerator = null;
             try {
@@ -22,29 +18,6 @@ namespace EZBlocker3.Audio.ComWrapper {
                     Marshal.ReleaseComObject(sessionEnumerator);
                 throw;
             }
-        }
-
-        class Enumerable : IEnumerable<AudioSession> {
-            private AudioSessionManager _audioSessionManager;
-
-            public Enumerable(AudioSessionManager audioSessionManager) {
-                _audioSessionManager = audioSessionManager;
-            }
-
-            public IEnumerator<AudioSession> GetEnumerator() {
-                IAudioSessionEnumerator? sessionEnumerator = null;
-                try {
-                    Marshal.ThrowExceptionForHR(_audioSessionManager.ComObject.GetSessionEnumerator(out sessionEnumerator));
-                    if (sessionEnumerator == null)
-                        return System.Linq.Enumerable.Empty<AudioSession>().GetEnumerator();
-                    return new AudioSessionEnumerator(sessionEnumerator);
-                } finally {
-                    if (sessionEnumerator != null)
-                        Marshal.ReleaseComObject(sessionEnumerator);
-                }
-            }
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
 
     }
