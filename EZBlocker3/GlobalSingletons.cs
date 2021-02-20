343dd75bfa7fe302daf7a853dd9e8018e0d23b79
OpenByte <development.openbyte@gmail.com>
2021-02-20 05:36:38 +0100

    Minor changes

--- EZBlocker3/GlobalSingletons.cs
+++ EZBlocker3/GlobalSingletons.cs
@@ -1,12 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Net.Http;
+﻿using System.Net.Http;
 
 namespace EZBlocker3 {
     public static class GlobalSingletons {
         private static HttpClient? _httpClient;
 
-        [SuppressMessage("Performance", "U2U1025:Avoid instantiating HttpClient", Justification = "HttpClient is only instantiated once.")]
-        public static HttpClient HttpClient => _httpClient ??= new HttpClient();
+        public static HttpClient HttpClient => _httpClient ??= new();
 
         public static void Dispose() {
             _httpClient?.Dispose();
